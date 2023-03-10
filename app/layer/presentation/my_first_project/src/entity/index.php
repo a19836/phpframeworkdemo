bbdb18a6274ad32f35f5a626d38d48d715296c00
a19836 <a19836@gmail.com>
2023-03-10 11:15:41 +0000

    updating new demo

--- app/layer/presentation/my_first_project/src/entity/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-//PAGE PROPERTIES:
-$EVC->getCMSLayer()->getCMSPagePropertyLayer()->setParseFullHtml(true);
-$EVC->getCMSLayer()->getCMSPagePropertyLayer()->setFilterByPermission(false);
-$EVC->getCMSLayer()->getCMSPagePropertyLayer()->setIncludeBlocksWhenCallingResources(false);
-?>
