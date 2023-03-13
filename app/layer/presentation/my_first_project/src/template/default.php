0a62e3529adbaf97d9d0ed5360b29dae6520ced5
a19836 <a19836@gmail.com>
2023-03-13 18:41:46 +0000

    updating new demo

--- app/layer/presentation/my_first_project/src/template/default.php
+++ app/layer/presentation/my_first_project/src/template/default.php
@@ -1,28 +1,22 @@
 <?php 
-
-//Template params:
+//TEMPLATE PARAMS:
 $EVC->getCMSLayer()->getCMSTemplateLayer()->setParam("Page Title", "This is the default template...");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="icon" href="data:;base64,=" />
-	</head>
-	<body>
-		<h1 class="title" style="text-align:center">
-			<?= $EVC->getCMSLayer()->getCMSTemplateLayer()->getParam("Page Title");
-			?>
-		</h1>
-		<div class="menu" style="background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);">
-			<?
-			echo $EVC->getCMSLayer()->getCMSTemplateLayer()->renderRegion("Menu");
-			?>
-		</div>
-		<div class="content" style="padding: 20px;">
-			<?
-			echo $EVC->getCMSLayer()->getCMSTemplateLayer()->renderRegion("Content");
-			?>
-		</div>
-	</body>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="icon" href="data:;base64,=" />
+</head>
+<body>
+	<h1 class="title" style="text-align:center">
+		<?= $EVC->getCMSLayer()->getCMSTemplateLayer()->getParam("Page Title"); ?>
+	</h1>
+	<div class="menu" style="background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);">
+		<?= $EVC->getCMSLayer()->getCMSTemplateLayer()->renderRegion("Menu"); ?>
+	</div>
+	<div class="content" style="padding: 20px;">
+		<?= $EVC->getCMSLayer()->getCMSTemplateLayer()->renderRegion("Content"); ?>
+	</div>
+</body>
 </html>
