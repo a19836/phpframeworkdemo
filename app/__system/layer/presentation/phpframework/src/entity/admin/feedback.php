f1131c230686779887c803f1d86cbea887b70dfd
a19836 <a19836@gmail.com>
2025-05-08 15:12:50 +0100

    optimizing

--- app/__system/layer/presentation/phpframework/src/entity/admin/feedback.php
+++ app/__system/layer/presentation/phpframework/src/entity/admin/feedback.php
@@ -5,4 +5,4 @@
  * Please note that this code belongs to the Bloxtor framework and must comply with the Bloxtor license.
  * If you do not accept these provisions, or if the Bloxtor License is not present or cannot be found, you are not entitled to use this code and must stop and delete it immediately.
  */
-$UserAuthenticationHandler->checkPresentationFileAuthentication($entity_path, "access"); $popup = isset($_GET["popup"]) ? $_GET["popup"] : null; $status = isset($_GET["status"]) ? $_GET["status"] : null; $msg = isset($_GET["msg"]) ? $_GET["msg"] : null; if ($status) $status_message = $msg ? $msg : "Feedback sent successfully!"; else $error_message = $msg ? $msg : "Feedback not sent! Please try again..."; ?>
+$UserAuthenticationHandler->checkPresentationFileAuthentication($entity_path, "access"); $popup = isset($_GET["popup"]) ? $_GET["popup"] : null; $status = isset($_GET["status"]) ? $_GET["status"] : null; $msg = isset($_GET["msg"]) ? $_GET["msg"] : null; if (isset($status)) {if ($status) $status_message = $msg ? $msg : "Feedback sent successfully!"; else $error_message = $msg ? $msg : "Feedback not sent! Please try again...";} ?>
